5c7db86a50f7bdad7a31ad69f7ddf259286ec52e
Luca Bacchi <bacchilu@gmail.com>
2024-12-03 10:40:24 +0100

    Startup refactoring

--- docker/UP.sh
+++ docker/UP.sh
@@ -2,12 +2,19 @@
 
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
+export $(grep -v '^#' .env | xargs)
+
+create_directories() {
+    mkdir -p $UPLOAD_DIRECTORY
+}
 
 ENVIRONMENT=$1
 
 if [ "$ENVIRONMENT" == "DEV" ]; then
+    create_directories
     docker compose --env-file .env up dev
 elif [ "$ENVIRONMENT" == "PROD" ]; then
+    create_directories
     docker compose --env-file .env up -d prod
 else
     echo "Invalid environment specified: $ENVIRONMENT"

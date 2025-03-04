e4deb8dd640be2c47080ec6a0ed6822e4e885434
Luca Bacchi <bacchilu@gmail.com>
2025-03-04 10:53:58 +0100

    Refactoring

--- src/libs/s3.py
+++ src/libs/s3.py
@@ -4,20 +4,24 @@ from typing import IO
 from aws.aios3 import Bucket
 
 
-async def get(key: str):
-    async with Bucket.create_obj("life365") as bucket:
-        with io.BytesIO() as fp:
-            await bucket.download_fileobj(fp, key)
-            return fp.getvalue()
+class S3:
+    @staticmethod
+    async def get(key: str):
+        async with Bucket.create_obj("life365") as bucket:
+            with io.BytesIO() as fp:
+                await bucket.download_fileobj(fp, key)
+                return fp.getvalue()
 
+    @staticmethod
+    async def upload(filename: str, file: IO[bytes], sub_path: str):
+        async with Bucket.create_obj("life365") as bucket:
+            file.seek(0)
+            await bucket.upload_fileobj(file, f"{sub_path}/{filename}")
+            return (
+                f"https://life365.s3.eu-central-1.amazonaws.com/{sub_path}/{filename}"
+            )
 
-async def upload(filename: str, file: IO[bytes], sub_path: str):
-    async with Bucket.create_obj("life365") as bucket:
-        file.seek(0)
-        await bucket.upload_fileobj(file, f"{sub_path}/{filename}")
-        return f"https://life365.s3.eu-central-1.amazonaws.com/{sub_path}/{filename}"
-
-
-async def delete(key: str):
-    async with Bucket.create_obj("life365") as bucket:
-        await bucket.delete_objects(key)
+    @staticmethod
+    async def delete(key: str):
+        async with Bucket.create_obj("life365") as bucket:
+            await bucket.delete_objects(key)
--- src/main.py
+++ src/main.py
@@ -1,47 +1,15 @@
-from enum import Enum
 import os
-from typing import IO
 
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.responses import Response
 
-from libs import fs, s3
-from libs.utils import guess_content_type, check_key
+from libs import getStorage
+from libs.utils import check_key, guess_content_type
 
-
-class StorageEnum(str, Enum):
-    FS = "FS"
-    S3 = "S3"
-
-
-STORAGE = StorageEnum(os.getenv("STORAGE", "S3"))
+S = getStorage()
 
 
 app = FastAPI()
-
-
-async def upload(filename: str, file: IO[bytes], key: str):
-    if STORAGE == StorageEnum.FS:
-        return await fs.upload(filename, file, key)
-    if STORAGE == StorageEnum.S3:
-        return await s3.upload(filename, file, key)
-    assert False
-
-
-async def get(key: str):
-    if STORAGE == StorageEnum.FS:
-        return await fs.get(key)
-    if STORAGE == StorageEnum.S3:
-        return await s3.get(key)
-    assert False
-
-
-async def delete(key: str):
-    if STORAGE == StorageEnum.FS:
-        return await fs.delete(key)
-    if STORAGE == StorageEnum.S3:
-        return await s3.delete(key)
-    assert False
 
 
 @app.post("/{key:path}")
@@ -49,7 +17,7 @@ async def upload_file(key: str, file: UploadFile = File(...)):
     assert file.filename is not None
     try:
         key = check_key(key)
-        file_location = await upload(file.filename, file.file, key)
+        file_location = await S.upload(file.filename, file.file, key)
         return {"filename": file.filename, "location": file_location}
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
@@ -59,7 +27,7 @@ async def upload_file(key: str, file: UploadFile = File(...)):
 async def get_file(key: str, filename: str):
     try:
         key = check_key(key)
-        file_data = await get(os.path.join(key, filename))
+        file_data = await S.get(os.path.join(key, filename))
         return Response(
             file_data,
             media_type=guess_content_type(file_data),
@@ -73,5 +41,5 @@ async def get_file(key: str, filename: str):
 async def delete_file_with_path(key: str):
     if ".." in key or key.startswith("/"):
         raise HTTPException(status_code=400, detail="Invalid sub-path")
-    await delete(key)
+    await S.delete(key)
     return {"filename": key}

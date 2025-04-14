c28d0c38f09716887735e346bacc0dd6a18c362c
Luca Bacchi <bacchilu@gmail.com>
2025-04-14 15:18:26 +0200

    Refactoring

--- src/data_mapper/s3.py
+++ src/data_mapper/s3.py
@@ -13,28 +13,28 @@ AWS_SECRET_ACCESS_KEY = os.getenv(
 REGION_NAME = os.getenv("REGION_NAME", "eu-central-1")
 
 
+def get_bucket():
+    return Bucket.create_obj(
+        S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME
+    )
+
+
 class S3(StorageProtocol):
     @staticmethod
     async def get(key: str):
-        async with Bucket.create_obj(
-            S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME
-        ) as bucket:
+        async with get_bucket() as bucket:
             with io.BytesIO() as fp:
                 await bucket.download_fileobj(fp, key)
                 return fp.getvalue()
 
     @staticmethod
     async def upload(filename: str, file: IO[bytes], sub_path: str):
-        async with Bucket.create_obj(
-            S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME
-        ) as bucket:
+        async with get_bucket() as bucket:
             file.seek(0)
             await bucket.upload_fileobj(file, f"{sub_path}/{filename}")
             return f"https://{S3_BUCKET}.s3.eu-central-1.amazonaws.com/{sub_path}/{filename}"
 
     @staticmethod
     async def delete(key: str):
-        async with Bucket.create_obj(
-            S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME
-        ) as bucket:
+        async with get_bucket() as bucket:
             await bucket.delete_objects(key)

6370454dabc0ce49347a6913a80320d2db5c4444
Luca Bacchi <bacchilu@gmail.com>
2025-06-12 11:27:58 +0200

    Utility scripts

---
+++ BUILD.sh
@@ -0,0 +1,2 @@
+python3 -m venv ENV
+./ENV/bin/pip3 install -r src/requirements-lock.txt
---
+++ UPDATE.sh
@@ -0,0 +1,3 @@
+python3 -m venv ENV
+./ENV/bin/pip3 install -r src/requirements.txt
+./ENV/bin/pip3 freeze > src/requirements-lock.txt

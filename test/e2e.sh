fc18accd6bf40defdcec58291ce489bbbfc82f65
Luca Bacchi <bacchilu@gmail.com>
2025-10-29 15:22:53 +0100

    Add separators to e2e curl script output
      Increase readability of successive curl responses during end-to-end runs.

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 curl -H 'Accept: application/json' -F 'file=@./dante.txt;type=text/plain' http://0.0.0.0:8000/xxx/yyy | jq
+echo "------------------------------------------------------------"
 curl http://0.0.0.0:8000/xxx/yyy/dante.txt | { cat; printf '\n'; }
+echo "------------------------------------------------------------"
 curl http://0.0.0.0:8000/xxx/yyy/ | jq
+echo "------------------------------------------------------------"
 curl --request DELETE http://0.0.0.0:8000/xxx/yyy/dante.txt | jq

6ae64e9c9d52199b0521e12ab8ceeb3473f48d4a
Luca Bacchi <bacchilu@gmail.com>
2025-10-29 14:06:49 +0100

    Inline service creation in router module

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
 
-from .router import create_router
-from .service import Service
-from .utils import getConfiguredStorage
-
-storage = getConfiguredStorage()
-service = Service(storage)
+from .router import router
 
 engine = FastAPI()
-engine.include_router(create_router(service))
+engine.include_router(router)
--- src/app/router.py
+++ src/app/router.py
@@ -1,50 +1,54 @@
-__all__ = ["create_router"]
+__all__ = ["router"]
 
 import os
 
 from fastapi import APIRouter, File, HTTPException, Response, UploadFile, status
 
 from .service import Service
-from .utils import guess_content_type
+from .utils import getConfiguredStorage, guess_content_type
+
+storage = getConfiguredStorage()
+service = Service(storage)
 
 
-def create_router(service: Service) -> APIRouter:
-    router = APIRouter()
+router = APIRouter()
 
-    @router.post("/{key:path}", status_code=status.HTTP_201_CREATED)
-    async def upload_file(response: Response, key: str, file: UploadFile = File(...)):
-        assert file.filename is not None
-        try:
-            file_location = await service.upload_file(file.filename, file.file, key)
-            response.headers["Location"] = file_location
-            return {"filename": file.filename, "location": file_location}
-        except Exception as e:
-            raise HTTPException(status_code=404, detail=str(e))
 
-    @router.get("/{key:path}/")
-    async def list_files(key: str):
-        try:
-            return await service.list_files(key)
-        except Exception as e:
-            raise HTTPException(status_code=404, detail=str(e))
+@router.post("/{key:path}", status_code=status.HTTP_201_CREATED)
+async def upload_file(response: Response, key: str, file: UploadFile = File(...)):
+    assert file.filename is not None
+    try:
+        file_location = await service.upload_file(file.filename, file.file, key)
+        response.headers["Location"] = file_location
+        return {"filename": file.filename, "location": file_location}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
-    @router.get("/{key:path}/{filename}")
-    async def get_file(key: str, filename: str):
-        try:
-            file_data = await service.get_file(os.path.join(key, filename))
-            return Response(
-                file_data,
-                media_type=guess_content_type(file_data),
-            )
-        except Exception as e:
-            raise HTTPException(status_code=404, detail=str(e))
 
-    @router.delete("/{key:path}")
-    async def delete_file_with_path(key: str):
-        try:
-            await service.delete_file(key)
-            return {"message": "File deleted successfully"}
-        except Exception as e:
-            raise HTTPException(status_code=404, detail=str(e))
+@router.get("/{key:path}/")
+async def list_files(key: str):
+    try:
+        return await service.list_files(key)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
-    return router
+
+@router.get("/{key:path}/{filename}")
+async def get_file(key: str, filename: str):
+    try:
+        file_data = await service.get_file(os.path.join(key, filename))
+        return Response(
+            file_data,
+            media_type=guess_content_type(file_data),
+        )
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+
+@router.delete("/{key:path}")
+async def delete_file_with_path(key: str):
+    try:
+        await service.delete_file(key)
+        return {"message": "File deleted successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))

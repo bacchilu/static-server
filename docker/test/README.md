bcbbce6ae63ee9bde5162bb7a54b7ef01e0d1849
Luca Bacchi <bacchilu@gmail.com>
2025-10-29 11:29:33 +0100

    Remove unused docker test harness
      Drop the docker/test image docs and entrypoint scripts that were
      only used for local experiments.

--- docker/test/README.md
+++
@@ -1,4 +0,0 @@
-    docker build -t test .
-
-    docker run --rm -it -e TEST=yyy test
-    docker run --rm -it -e TEST=yyy --user 1111:1111 test
--- docker/test/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USER_ID=$(id -u)
-GROUP_ID=$(id -g)
-
-echo "Environment Variables: $TEST, $PATH"
-echo "User ID: $USER_ID, Group ID: $GROUP_ID"
-
-exec "$@"
